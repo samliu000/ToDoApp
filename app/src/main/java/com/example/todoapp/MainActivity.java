70ffdc52d1bc1e39fdfd2a8f27fbee3c8f47324d
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-09-15 20:34:41 -0700

    Episode 2 done

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.todoapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to gym");
+        items.add("Have fun");
     }
 }
